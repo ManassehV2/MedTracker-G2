3549c4730e262cd9afc911abff7b61595b9e02bb
FevenBelay23 <91492132+FevenBelay23@users.noreply.github.com>
2022-12-09 22:51:27 +0300

    Adding Models

---
+++ src/MedAdvisor.Models/models/Diagnosis.cs
@@ -0,0 +1,8 @@
+namespace MedAdvisor.Models;
+public class Diagnosis
+{
+    public string userId{get;set;}
+    public string DiagnosisName{ get; set;}
+    public string Diagnosiscode{ get; set;}
+
+}
---
+++ src/MedAdvisor.Models/models/Vaccines.cs
@@ -0,0 +1,8 @@
+namespace MedAdvisor.Models;
+public class Vaccines
+{
+    public string userId{get;set;}
+    public string VaccinesName{ get; set;}
+    public string Vaccinescode{ get; set;}
+
+}
