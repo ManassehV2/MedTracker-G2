8f5a6a2ff6539992cdc4b206b57c68026c3630f9
FevenBelay23 <91492132+FevenBelay23@users.noreply.github.com>
2022-12-14 09:32:53 +0300

    Adding Models

--- src/MedAdvisor.Models/models/Diagnosis.cs
+++ src/MedAdvisor.Models/models/Diagnosis.cs
@@ -1,8 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MedAdvisor.Models;
 public class Diagnosis
 {
-    public string userId{get;set;}
-    public string DiagnosisName{ get; set;}
-    public string Diagnosiscode{ get; set;}
+    public int Id { get; set; }
+    public int UserId { get; set; }
+
+    [Required]
+    [StringLength(20, MinimumLength = 4, ErrorMessage = "Must be at least 4 characters long.")]
+    public string? Name { get; set; }
+
+    [Required]
+    [StringLength(20, MinimumLength = 4, ErrorMessage = "Must be at least 4 characters long.")]
+    public string? Code { get; set; }
 
 }
