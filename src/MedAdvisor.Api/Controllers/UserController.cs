a513556f7cbc0477e4895812f6c894a21d686208
FEVEN <fevenbelay942@gmail.com>
2023-02-13 15:29:38 +0300

    UserProfile

--- src/MedAdvisor.Api/Controllers/UserController.cs
+++ src/MedAdvisor.Api/Controllers/UserController.cs
@@ -27,6 +27,26 @@ public class UserController : ControllerBase
 
     public IActionResult getUser([FromHeader] string Authorization)
     {
+        try
+        {
+            int userId = UserFromToken.getId(Authorization);
+            var user = _userRepository.GetUser(userId);
+            // UserProfileData userMap = _mapper.Map<UserProfileData>(_userRepository.GetUser(userId));
+            
+            if (user == null)
+            {
+                ModelState.AddModelError("", "bad request");
+                return BadRequest(ModelState);
+            }
+            
+            return Ok(user);
+        }
+        catch (Exception ex)
+        {
+            ModelState.AddModelError("Error", ex.Message);
+            return BadRequest(ModelState);
+
+        }
         
     }
 
