8646abec43eac75d5b3a8327d92758951a0fd4e3
FEVEN <fevenbelay942@gmail.com>
2023-02-13 15:35:23 +0300

    UserProfile

--- src/MedAdvisor.Api/Controllers/UserFromTokens.cs
+++ src/MedAdvisor.Api/Controllers/UserFromTokens.cs
@@ -8,8 +8,21 @@ namespace MedAdvisor.Api.Controllers
         public static int getId(string token)
         {
             // 
+            var streamList = token.Split(" ").ToList();
+            string stream;
+         
+            if (streamList.Count == 0){
+                throw new System.Exception("token is empty");
+            }
+            else if (streamList.Count == 1) {
+                stream = streamList[0];
+            }
+            else{
+                stream = streamList[streamList.Count - 1];
+            }
+        
             
-
+            
         }
     }
 }
