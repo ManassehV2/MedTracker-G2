6f8fd8931f06f1f8aad8a5fb97ccab1498b397ce
yadishman <96906829+yadishman@users.noreply.github.com>
2023-02-13 18:25:56 +0300

    vaccine controller get method finished

---
+++ src/MedAdvisor.Api/Controllers/VaccineController.cs
@@ -0,0 +1,52 @@
+using MedAdvisor.Api.DataClass;
+using MedAdvisor.DataAccess.MySql;
+using MedAdvisor.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedAdvisor.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VaccineController : Controller
+    {
+        private readonly IUserVaccineRepository _repo;
+        public VaccineController(IUserVaccineRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Vaccine>))]
+        public IActionResult GetVaccine([FromHeader] string Authorization)
+
+        {
+            try
+            {
+                int userid = UserFromToken.getId(Authorization);
+
+
+                ICollection<Vaccine> result = _repo.GetUserVaccines(userid);
+
+
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+
+                return BadRequest(ModelState);
+
+            }
+
+        }
+
+
+        
+    }
+}
