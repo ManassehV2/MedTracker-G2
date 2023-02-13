18153d06680b86fb1f479c099ed5c2727c32563c
FEVEN <fevenbelay942@gmail.com>
2023-02-13 15:28:39 +0300

    UserProfile

--- src/MedAdvisor.Api/Controllers/UserController.cs
+++ src/MedAdvisor.Api/Controllers/UserController.cs
@@ -11,6 +11,26 @@ namespace MedAdvisor.Api.Controllers;
 [Route("api/user")]
 public class UserController : ControllerBase
 {
+     private readonly IConfiguration _config;
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public UserController(IConfiguration config, IUserRepository userRepository, IMapper mapper)
+
+    {
+        _config = config;
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+
+    public IActionResult getUser([FromHeader] string Authorization)
+    {
+        
+    }
+
+    
 
    
 }
