50482f71ffaafb6b2c605c09afe55cff3ba39511
yadishman <96906829+yadishman@users.noreply.github.com>
2023-02-13 18:28:09 +0300

    vaccine controller delete method finished

--- src/MedAdvisor.Api/Controllers/VaccineController.cs
+++ src/MedAdvisor.Api/Controllers/VaccineController.cs
@@ -78,9 +78,38 @@ namespace MedAdvisor.Api.Controllers
 
 
         }
-
         
+        [HttpDelete]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult DeleteVaccine([FromBody] VaccineDeleteData dData, [FromHeader] string Authorization)
 
-        
+        {
+            if (dData == null)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                int userId = UserFromToken.getId(Authorization);
+
+                bool result = _repo.RemoveVaccines(userId, dData.vaccineId);
+                if (!result)
+                {
+                    ModelState.AddModelError("", "bad request");
+                    return BadRequest(ModelState);
+
+                }
+                return NoContent();
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return BadRequest(ModelState);
+
+            }
+        }
     }
 }
