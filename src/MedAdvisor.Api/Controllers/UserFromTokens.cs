7a897631dbaa5f786b790ae5c970db5502704084
FEVEN <fevenbelay942@gmail.com>
2023-02-13 15:10:57 +0300

    UserProfile

--- src/MedAdvisor.Api/Controllers/UserFromTokens.cs
+++ src/MedAdvisor.Api/Controllers/UserFromTokens.cs
@@ -5,6 +5,38 @@ namespace MedAdvisor.Api.Controllers
 {
     public class UserFromToken
     {
+        public static int getId(string token)
+        {
+            // 
+            var streamList = token.Split(" ").ToList();
+            string stream;
+         
+            if (streamList.Count == 0){
+                throw new System.Exception("token is empty");
+            }
+            else if (streamList.Count == 1) {
+                stream = streamList[0];
+            }
+            else{
+                stream = streamList[streamList.Count - 1];
+            }
         
+            
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(stream);
+            var tokenS = jsonToken as JwtSecurityToken;
+            if (tokenS == null){
+                throw new ApplicationException("Invalid token");
+            }
+            
+            
+            var jti = tokenS.Claims.First(claim => claim.Type.ToString() == ClaimTypes.Name.ToString()).Value;
+            
+
+            int userId = int.Parse(jti);
+            
+            return userId;
+
+        }
     }
 }
