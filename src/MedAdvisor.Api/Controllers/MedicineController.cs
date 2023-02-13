a08a3feb955728eeaf303e19393a46379bb6e960
yadishman <96906829+yadishman@users.noreply.github.com>
2023-02-13 18:40:27 +0300

    medicine controller post method finished

--- src/MedAdvisor.Api/Controllers/MedicineController.cs
+++ src/MedAdvisor.Api/Controllers/MedicineController.cs
@@ -45,6 +45,41 @@ namespace MedAdvisor.Api.Controllers
             }
 
         }
+        
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+
+        public IActionResult AddMedicine([FromBody] MedicineData data, [FromHeader] string Authorization)
+
+        {
+            if (data == null)
+            {
+
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                int userId = UserFromToken.getId(Authorization);
+                bool result = _repo.AddMedicine(userId, data.medicineId);
+                if (!result)
+                {
+                    ModelState.AddModelError("", "bad request");
+                    return BadRequest(ModelState);
+                }
+                return Ok("Successfully added");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return BadRequest(ModelState);
+
+            }
+
+
+        }
+
+        
 
 
         
