2b23a1094c4ff0b5c8e743deaa55f06bf21b0973
FEVEN <fevenbelay942@gmail.com>
2023-02-13 15:31:36 +0300

    UserProfile

--- src/MedAdvisor.Api/Controllers/UserController.cs
+++ src/MedAdvisor.Api/Controllers/UserController.cs
@@ -11,7 +11,8 @@ namespace MedAdvisor.Api.Controllers;
 [Route("api/user")]
 public class UserController : ControllerBase
 {
-     private readonly IConfiguration _config;
+
+    private readonly IConfiguration _config;
     private readonly IUserRepository _userRepository;
     private readonly IMapper _mapper;
 
@@ -47,16 +48,66 @@ public class UserController : ControllerBase
             return BadRequest(ModelState);
 
         }
-        
     }
 
     [HttpPatch]
     public IActionResult updateUser(UserProfileData profile, [FromHeader] string Authorization)
     {
-        
+        try
+        {
+            int userId = UserFromToken.getId(Authorization);
+            var newUser = new User() {
+                  Id = profile.Id,
+                FirstName = profile.FirstName,
+                LastName = profile.LastName,
+
+                DateOfBirth = profile.DateOfBirth,
+
+                Gender = (User.Gendertypes)(Gendertypes)profile.Gender,
+                Ssn = profile.Ssn,
+                Nationality = profile.Nationality,
+                Telephone = profile.Telephone,
+
+                OrganDonor = profile.OrganDonor,
+
+                Postnr = profile.Postnr,
+                City = profile.City,
+                Land = profile.Land,
+
+                StreetAddress = profile.StreetAddress,
+
+                TypeOfInsurance = profile.TypeOfInsurance,
+                InsuranceCompany = profile.InsuranceCompany,
+                AlarmTel = profile.AlarmTel,
+
+                EmergencyContactName1 = profile.EmergencyContactName1,
+
+                EmergencyContactPhone1 = profile.EmergencyContactPhone1,
+                EmergencyContactRel1 = profile.EmergencyContactRel1,
+                EmergencyContactName2 = profile.EmergencyContactName2,
+
+                EmergencyContactPhone2 = profile.EmergencyContactPhone2,
+                EmergencyContactRel2 = profile.EmergencyContactRel2,
+                Other = profile.Other,
+
+            };
+
+            bool result = _userRepository.UpdateUser(userId, newUser);
+
+            if (!result)
+            {
+                ModelState.AddModelError("", "bad request");
+                return BadRequest(ModelState);
+
+            }
+
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            ModelState.AddModelError("", ex.Message);
+            return BadRequest(ModelState);
+
+        }
     }
-
-    
-
-   
 }
