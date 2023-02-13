d4f3def5696350929a31b92a8f2906262c865ab2
FEVEN <fevenbelay942@gmail.com>
2023-02-13 15:27:23 +0300

    UserProfile

---
+++ src/MedAdvisor.Api/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+using AutoMapper;
+using MedAdvisor.DataAccess.MySql;
+using MedAdvisor.Models.Models;
+using static MedAdvisor.Api.UserProfileData;
+
+namespace MedAdvisor.Api.Controllers;
+
+[ApiController]
+[Route("api/user")]
+public class UserController : ControllerBase
+{
+
+   
+}
