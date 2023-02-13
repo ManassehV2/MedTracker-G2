5b566e40eaa8392f28f3198957009c13f7fd3f78
FEVEN <fevenbelay942@gmail.com>
2023-02-13 15:04:45 +0300

    UserProfile

---
+++ src/MedAdvisor.Api/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+using AutoMapper;
+using MedAdvisor.DataAccess.MySql;
+using MedAdvisor.Models.Models;
+using static MedAdvisor.Api.UserProfileData;
+
+namespace MedAdvisor.Api.Controllers;
+
+[ApiController]
+[Route("api/user")]
+public class UserController : ControllerBase
+{
+
+    private readonly IConfiguration _config;
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public UserController(IConfiguration config, IUserRepository userRepository, IMapper mapper)
+
+    {
+        _config = config;
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    
+}
