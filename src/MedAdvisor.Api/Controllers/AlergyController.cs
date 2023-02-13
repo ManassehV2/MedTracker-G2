3d38c987777391d6efd995f6a57b5eb2064e93cc
zerabruck <zerabruckm@gmail.com>
2023-02-13 17:19:05 +0300

    delete method done for allergy controller

--- src/MedAdvisor.Api/Controllers/AlergyController.cs
+++ src/MedAdvisor.Api/Controllers/AlergyController.cs
@@ -78,7 +78,38 @@ namespace MedAdvisor.Api.Controllers
 
 
         }
+        [HttpDelete]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult DeleteAllergy([FromBody] AllergyDeleteData dData,[FromHeader] string Authorization)
 
-        
+        {
+            if (dData == null)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                int userId = UserFromToken.getId(Authorization);
+
+                bool result = _repo.RemoveAllergies(userId, dData.AllergyId);
+                if (!result)
+                {
+                    ModelState.AddModelError("", "bad request");
+                    return BadRequest(ModelState);
+
+                }
+                return NoContent();
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return BadRequest(ModelState);
+
+            }
+        }
+    
         }
 }
