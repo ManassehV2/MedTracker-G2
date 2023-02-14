0f49f746d89b845fa585d14645f849c37124435e
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2023-02-14 13:28:05 +0300

    Update AuthController.Test.cs

---
+++ test/MedAdvisor.API.Test/ControllerUnitTests/AuthController.Test.cs
@@ -0,0 +1,97 @@
+using System.Net;
+using FakeItEasy;
+using FluentAssertions;
+using MedAdvisor.Api;
+using MedAdvisor.Api.Controllers;
+using MedAdvisor.DataAccess.MySql;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace MedAdvisor.API.Test;
+
+public class AuthControllerTests
+{
+    public readonly IConfiguration _config;
+    public readonly IUserRepository _repository;
+    public AuthControllerTests()
+    {
+        _config = A.Fake<IConfiguration>();
+        _repository = A.Fake<IUserRepository>();
+
+    }
+
+    [Fact]
+    public void AuthController_Signup_ReturnOK()
+    {
+        //arrange
+        var authController = new AuthController(_config,_repository);
+        var userDto = A.Fake<UserDto>();
+
+        //Act
+        var result = authController.signup(userDto);
+
+        //Assert
+        result.Should().NotBeNull();
+
+        result.Should().BeOfType(typeof(OkResult));
+
+
+    }
+    [Fact]
+    public void AuthController_Login_ReturnOK()
+    {
+        //Arrange
+        var authController = new AuthController(_config,_repository);
+        var userDto = A.Fake<UserDto>();
+
+        //Act
+        var result = authController.login(userDto);
+
+        //Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType(typeof(OkResult));
+        
+
+    }
+
+    [Fact]
+    public void AuthController_LoginWithWrongUsername_ReturnBadRequest()
+    {
+        //Arrange
+        var authController = new AuthController(_config,_repository);
+        var userDto = A.Fake<UserDto>();
+
+        //Act
+        authController.signup(userDto);
+
+        userDto.Email = "newEmail2@email.com";
+        var result = authController.login(userDto);
+
+        //Assert
+        result.Should().NotBeNull();
+
+        result.Should().BeOfType(typeof(BadRequestObjectResult));
+
+    }
+    [Fact]
+    public void AuthController_LoginWithWrongPassword_ReturnBadRequest()
+    {
+        //Arrange
+        var authController = new AuthController(_config,_repository);
+        var userDto = A.Fake<UserDto>();
+
+        //Act
+        authController.signup(userDto);
+
+        userDto.Password = "newPassword";
+        var result = authController.login(userDto);
+
+        //Assert
+        result.Should().NotBeNull();
+
+        result.Should().BeOfType(typeof(BadRequestObjectResult));
+
+    }
+
+}
